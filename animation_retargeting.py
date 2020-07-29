aa20b44dcb5907fcce7203be70a5c79aa5cfe811
CapnSquirrel <azapataa@g.ucla.edu>
2020-07-29 02:51:29 -0700

    mostly underway, animation retargeting not working

--- animation_retargeting.py
+++ animation_retargeting.py
@@ -6,22 +6,59 @@ def create_reference(file_path):
         maya.cmds.error("File path {0} doesn't exist".format(file_path))
         return
     
-    head, tail = os.path.split(file_path)
-    root, ext = os.path.splitext(tail)
-    if root[0].isdigit():
-        root = "anim" + root
+    _, file_name = os.path.split(file_path)
+    name, _ = os.path.splitext(file_name)
+    if name[0].isdigit():
+        name = "anim" + name
     
-    maya.cmds.file(file_path, r=True, ns=root)
-    return root
+    maya.cmds.file(file_path, r=True, ns=name)
+    return name
     
 def get_joints_from_namespace(ns):
     return maya.cmds.ls(ns + ":*", type="joint")
     
 def connect_attributes(src, dst, attr):
-    srcString = "{0}.{1}".format(src, attr)
-    dstString = "{0}.{1}".format(dst, attr)
-    maya.cmds.connectAttr(srcString, dstString, f=True)
+    src_string = "{0}.{1}".format(src, attr)
+    dst_string = "{0}.{1}".format(dst, attr)
+    maya.cmds.connectAttr(src_string, dst_string, f=True)
+    # maya.cmds.parentConstraint(src, dst, mo=True)
     
+def get_joint_name(jnt):
+    return jnt[jnt.find(":")+1:]
+    
+def connect_and_bake_joints(src_joints, dst_joints):
+    rot_attributes = ["rotateX", "rotateY", "rotateZ"]
+    trans_attributes = ["translateX", "translateY", "translateZ"]
+    
+    start_time = maya.cmds.playbackOptions(q = True, min = True)
+    end_time = maya.cmds.playbackOptions(q = True, max = True)
+
+    for dst_joint in dst_joints:
+        dst_joint_name = get_joint_name(dst_joint)
+        for src_joint in src_joints:
+            src_joint_name = get_joint_name(src_joint)
+            if dst_joint_name == src_joint_name:
+                # maya.cmds.parentConstraint(src_joint, dst_joint, mo=True)
+                # not sure about this
+                # for attr in rot_attributes:
+                #     connect_attributes(src_joint, dst_joint, attr)
+                # if dst_joint_name == "Hips":
+                #     for attr in trans_attributes:
+                #         connect_attributes(src_joint, dst_joint, attr)
+
+        maya.cmds.select(dst_joint)
+        maya.cmds.bakeResults(
+            simulation = True,
+            time = (start_time, end_time),
+            disableImplicitControl = True,
+            preserveOutsideKeys = True,
+            minimizeRotation = True,
+            shape = True
+        )
+          
+def sync_keyframes():
+    maya.cmds.findKeyframe(which="first")
+
 def run():
     # Create a new scene
     maya.cmds.file(new=True, f=True)
@@ -41,19 +78,17 @@ def run():
     character_joints = get_joints_from_namespace(character_namespace)
     
     # Attach animation to character
-    attributes = ["translateX", "translateY", "translateZ", "rotateX", "rotateY", "rotateZ"]
-    
-    for char_joint in character_joints:
-        cjoint_name = char_joint[char_joint.find(":")+1:]
-        for anim_joint in animation_joints:
-            ajoint_name = anim_joint[anim_joint.find(":")+1:]
-            if cjoint_name == ajoint_name:
-                print(anim_joint, char_joint)
-                for attr in attributes:
-                    connect_attributes(anim_joint, char_joint, attr)
+    connect_and_bake_joints(animation_joints, character_joints)
     
     # Remove references
-
+    # maya.cmds.file(animation_filename, rr = True)
+    # maya.cmds.file(character_filename, ir = True)
+    
     # Save a file
+    renamed_file = r"D:\CapnSquirrel\Development\TechArt\exercise\{0}_{1}".format(character_namespace, animation_namespace)
+    maya.cmds.file(rename=renamed_file)
+    # maya.cmds.file(save=True, f=True)
     
 run()
+
+
