d4899e53dc654a071b562bdc00d115b9c67d8a35
CapnSquirrel <azapataa@g.ucla.edu>
2020-07-29 16:58:34 -0700

    finished retargeting animation

--- animation_retargeting.py
+++ animation_retargeting.py
@@ -17,78 +17,67 @@ def create_reference(file_path):
 def get_joints_from_namespace(ns):
     return maya.cmds.ls(ns + ":*", type="joint")
     
-def connect_attributes(src, dst, attr):
-    src_string = "{0}.{1}".format(src, attr)
-    dst_string = "{0}.{1}".format(dst, attr)
-    maya.cmds.connectAttr(src_string, dst_string, f=True)
-    # maya.cmds.parentConstraint(src, dst, mo=True)
-    
 def get_joint_name(jnt):
     return jnt[jnt.find(":")+1:]
+
+def get_time_range(joint_list):
+    start_time = maya.cmds.findKeyframe(joint_list[0], which = "first")
+    end_time = maya.cmds.findKeyframe(joint_list[0], which = "last")
+    return start_time, end_time
+            
+def connect_joints(src_joints, dst_joints):
+    src_ns = src_joints[0].split(":")[0]
+    dst_ns = dst_joints[0].split(":")[0]
     
-def connect_and_bake_joints(src_joints, dst_joints):
-    rot_attributes = ["rotateX", "rotateY", "rotateZ"]
-    trans_attributes = ["translateX", "translateY", "translateZ"]
-    
-    start_time = maya.cmds.playbackOptions(q = True, min = True)
-    end_time = maya.cmds.playbackOptions(q = True, max = True)
+    for src_joint in src_joints:
+        src_joint_name = src_joint.split(":")[1]
+        dst_joint = "{}:{}".format(dst_ns, src_joint_name)
+        if maya.cmds.objExists(src_joint) and maya.cmds.objExists(dst_joint):
+            maya.cmds.orientConstraint(src_joint, dst_joint, mo=True)
+            if src_joint_name == "Hips":
+                maya.cmds.parentConstraint(src_joint, dst_joint, mo=True)
 
-    for dst_joint in dst_joints:
-        dst_joint_name = get_joint_name(dst_joint)
-        for src_joint in src_joints:
-            src_joint_name = get_joint_name(src_joint)
-            if dst_joint_name == src_joint_name:
-                # maya.cmds.parentConstraint(src_joint, dst_joint, mo=True)
-                # not sure about this
-                # for attr in rot_attributes:
-                #     connect_attributes(src_joint, dst_joint, attr)
-                # if dst_joint_name == "Hips":
-                #     for attr in trans_attributes:
-                #         connect_attributes(src_joint, dst_joint, attr)
-
-        maya.cmds.select(dst_joint)
-        maya.cmds.bakeResults(
-            simulation = True,
-            time = (start_time, end_time),
-            disableImplicitControl = True,
-            preserveOutsideKeys = True,
-            minimizeRotation = True,
-            shape = True
-        )
-          
-def sync_keyframes():
-    maya.cmds.findKeyframe(which="first")
-
-def run():
+def run(animation_filename, character_filename):
     # Create a new scene
     maya.cmds.file(new=True, f=True)
     
-    # Create char and anim namespaces
-    animation_filename = r"D:\CapnSquirrel\Development\TechArt\exercise\animations\maya\01_01.ma"
-    character_filename = "D:\CapnSquirrel\Development\TechArt\exercise\character.mb"
-    
-    # Bring in character
+    # Create char and anim namespaces and bring in character and animation
     character_namespace = create_reference(character_filename)
-    
-    # Bring in the animation
     animation_namespace = create_reference(animation_filename)
     
     # get a list of joints of both the anim and char
     animation_joints = get_joints_from_namespace(animation_namespace)
     character_joints = get_joints_from_namespace(character_namespace)
     
-    # Attach animation to character
-    connect_and_bake_joints(animation_joints, character_joints)
-    
+    #set animation start and end times
+    start_time, end_time = get_time_range(animation_joints)
+    maya.cmds.currentTime(start_time)
+    maya.cmds.viewFit(f = 0.5)
+    #connect joints
+    connect_joints(animation_joints, character_joints)
+
+    # bake animation
+    maya.cmds.select(character_joints)
+    maya.cmds.bakeResults(
+        simulation = True,
+        time = (start_time, end_time),
+        disableImplicitControl = True,
+        preserveOutsideKeys = True,
+        minimizeRotation = True,
+        shape = True
+    )
+
     # Remove references
-    # maya.cmds.file(animation_filename, rr = True)
-    # maya.cmds.file(character_filename, ir = True)
+    maya.cmds.file(animation_filename, rr = True)
     
     # Save a file
     renamed_file = r"D:\CapnSquirrel\Development\TechArt\exercise\{0}_{1}".format(character_namespace, animation_namespace)
     maya.cmds.file(rename=renamed_file)
-    # maya.cmds.file(save=True, f=True)
-    
-run()
+    maya.cmds.file(save=True, f=True)
 
+animations_dir = r"D:\CapnSquirrel\Development\TechArt\exercise\animations\maya"
+animation_files = [animations_dir + r"\01_" + ('%02d' %x) + ".ma" for x in range(1,11)]
+character_filename = "D:\CapnSquirrel\Development\TechArt\exercise\character.mb"
 
+for anim_file in animation_files:
+    run(anim_file, character_filename)
